3f24deec37c2caadd9a89b555472496f5afb5530
Fantazyhu <zsupko.tamas7456@gmail.com>
2026-02-13 14:03:55 +0100

    lejatszo mukodik

---
+++ movies/src/Player.tsx
@@ -0,0 +1,18 @@
+import { useState } from 'react'
+import './App.css'
+import Player from "./Player"
+
+function App() {
+  return (
+    <div>
+      <iframe
+        src="https://www.vidking.net/embed/movie/240"
+        width="100%"
+        height="600"
+        allowFullScreen
+      />
+    </div>
+  )
+}
+
+export default App
