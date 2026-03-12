9959b46890f81b03f60e5e872af253f5d4140b2f
lomorcsaszlo <laszlo.csomor22@gmail.com>
2026-03-12 09:20:02 +0100

    majd c# commit atnezes kell

--- c# console/MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
+++ c# console/MyApp/obj/Debug/net9.0/MyApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ece2c85d3da48af815c15bcc3f6e0d5c9356bdf2")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+24f6cde2405e9b36d7b4eb91e41e843dcddbf6f1")]
 [assembly: System.Reflection.AssemblyProductAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MyApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
